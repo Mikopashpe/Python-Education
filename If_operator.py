8e822514cc500fc56b5685544afe38115e0a8d67
Ekaterina Strakhova <mikopashpe@gmail.com>
2022-04-16 15:45:30 +0500

    Commiting if operator

--- If_operator.py
+++ If_operator.py
@@ -1,24 +1,24 @@
 '''
+В данном разделе происходит ознакомление с оператором if.
 '''
-#choice = input('Chooce your product: ')
+choice = input('Chooce your product: ')
 food_dict = {'ham' : 1.69, 'cheese' : 1.79, 'bread' : 0.36}
-#if choice == 'ham':
-   # print(food_dict['ham'], '£')
-#elif choice == 'cheese':
-#    print(food_dict['cheese'], '£')
-#elif choice == 'bread':
- #   print(food_dict['bread'], '£')
-#else:
- #   print('Bad choice!')
+if choice == 'ham':    # Данный код - пример множественного ветвления
+    print(food_dict['ham'], '£')
+elif choice == 'cheese':
+    print(food_dict['cheese'], '£')
+elif choice == 'bread':
+    print(food_dict['bread'], '£')
+else:
+    print('Bad choice!')
 
+try:    # Перехват и обработка исключений
+    print(food_dict[choice])
+except:
+    print('Bad choise!')
 
-#try:
-   # print(food_dict[choice])
-#except:
-    #print('Bad choise!')
-
-a = 'true' if 'word' else 'false'
+a = 'true' if 'word' else 'false'    # Непустые строки означают истину
 print(a)
 
-a = 'true' if '' else 'false'
+a = 'true' if '' else 'false'    # Пустые строки означают ложь
 print(a)
