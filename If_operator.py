75e3e2b0f3d71536177cc591a9533daf370f36af
Ekaterina Strakhova <mikopashpe@gmail.com>
2022-04-16 15:44:43 +0500

    Commiting if operator

---
+++ If_operator.py
@@ -0,0 +1,24 @@
+'''
+'''
+#choice = input('Chooce your product: ')
+food_dict = {'ham' : 1.69, 'cheese' : 1.79, 'bread' : 0.36}
+#if choice == 'ham':
+   # print(food_dict['ham'], '£')
+#elif choice == 'cheese':
+#    print(food_dict['cheese'], '£')
+#elif choice == 'bread':
+ #   print(food_dict['bread'], '£')
+#else:
+ #   print('Bad choice!')
+
+
+#try:
+   # print(food_dict[choice])
+#except:
+    #print('Bad choise!')
+
+a = 'true' if 'word' else 'false'
+print(a)
+
+a = 'true' if '' else 'false'
+print(a)
